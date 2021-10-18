057366924746f42f2ecf594043c6132e1141062f
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-10-18 22:37:34 +0000

    added comments to 'add' def

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def add (a: float, b: float):
+    """
+    Takes a float a and a float b as arguments and returns their sum as a float
+    Args:
+        a (float): number to be added
+        b (float): number to be added
+
+    Returns:
+        [float]: sum of a + b
+    """
     return (a + b)
