f910f5e4a46ac2f34f408162ac90e4e930de29d2
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-10-18 23:47:12 +0000

    added more detailed comments

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
 from typing import List
-"""[summary]
+"""
+[summary]
+Contains a single python function definition this time using
+complex type annotations (inpits List from typing for subscriptable List class)
 """
 
 
 def sum_list(input_list: List[float]) -> float:
     """[summary]
-
+    A type-annotated function sum_list which takes a list input_list of floats
+    as argument and returns their sum as a float.
     Args:
-        input_list (list[float]): [description]
+        input_list (list[float]): list of floats we are adding together
 
     Returns:
-        float: [description]
+        float: The sum of all the numbers in input_list
     """
     sum = 0
     for num in input_list:
