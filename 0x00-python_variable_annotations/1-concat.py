eb3a02237fe57eef145c416a788d2816d99c3e96
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-10-18 22:53:45 +0000

    added pycodestyle fixes

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -9,8 +9,8 @@ annotations
 def concat(str1: str, str2: str) -> str:
     """
     [summary]
-    A type-annotated function concat that takes a string str1 and a string str2 as
-    arguments and returns a concatenated string
+    A type-annotated function concat that takes a string str1
+    and a string str2 as arguments and returns a concatenated string
 
     Args:
         str1 (str): first string to be concatenated
