8c98f7afc361fb77c0d47ce079f177b04eb8a446
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-10-18 22:47:11 +0000

    added -> syntax to add function

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -6,7 +6,7 @@ annotations
 """
 
 
-def add(a: float, b: float):
+def add(a: float, b: float) -> float:
     """
     [summary]
     Takes a float a and a float b as arguments and returns their sum as a float
