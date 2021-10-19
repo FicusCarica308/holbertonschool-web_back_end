16a504d7ae94450616a6be54e81881b22d73086e
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-10-19 00:47:54 +0000

    added more detailed comments

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -11,10 +11,12 @@ def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """[summary]
 
     Args:
-        k (str): [description]
-        v (Union[int, float]): [description]
+        k (str): a string that will be returned unmodified
+        v (Union[int, float]): A number that can be either int or float
+        which will be squared
 
     Returns:
-        Tuple[str, float]: [description]
+        Tuple[str, float]: Returns a tuple with the first value being a str
+        and the second being a float (typed using typing Tuple class)
     """
     return (k, float(pow(v, 2)))
