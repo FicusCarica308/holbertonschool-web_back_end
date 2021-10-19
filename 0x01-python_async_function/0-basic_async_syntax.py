54efe07f56f890fd57bce7910dc9fc2de0465ba4
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-10-19 22:30:12 +0000

    added updated comments to file

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python3
 """[summary]
-    temporary summary
+    Module containing a python asynchronous coroutine
+    Imports:
+    asyncio - for await/async functionalitys
+    random - random number generations
 """
 import asyncio
-import typing
 from random import uniform
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """[summary]
+    """
+    
+     an asynchronous coroutine that takes in an integer argument (max_delay, with a default value of 10)
+     named wait_random that waits for a random delay between 0 and max_delay (included and float value)
+     seconds and eventually returns it.
 
     Args:
-        max_delay (int, optional): [description]. Defaults to 10.
+        max_delay (int, optional): A user given integer used to determine the
+        length of the sleep. Defaults to 10.
 
     Returns:
-        [type]: [description]
+        [float]: returns the amount in seconds the program slept for
     """
     delay = uniform(0, max_delay)
     await asyncio.sleep(delay)
