df899ca370cf6449b2de64c707a571c901b4fa96
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-10-20 00:57:49 +0000

    added task 2

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""[summary]
+"""
+
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """[summary]
+
+    Args:
+        n (int): [description]
+        max_delay (int): [description]
+
+    Returns:
+        float: [description]
+    """
+    start = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end = time.time()
+    return ((start + end) / n)
