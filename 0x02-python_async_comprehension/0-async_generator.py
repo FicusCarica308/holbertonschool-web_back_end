c2061c11b8570d4890b458914b36b09663be4121
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-10-20 23:03:06 +0000

    fixed a few pycodestyle errors

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,8 +9,8 @@ from typing import Generator
 
 async def async_generator() -> Generator[float, None, None]:
     """[summary]
-    A coroutine that will loop 10 times, each time asynchronously wait 1 second, 
-    then yield a random number between 0 and 10.
+    A coroutine that will loop 10 times, each time asynchronously wait 1
+    second, then yield a random number between 0 and 10.
     Yields:
         Generator[float, None, None]: A random generated float number
         typed using the typing librarys built in Generator Type
