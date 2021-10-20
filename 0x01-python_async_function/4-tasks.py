a1e614ea09919d3796bf2b334653158a935b3e5a
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-10-20 01:27:44 +0000

    added pycodestyle fixes

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -11,8 +11,9 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """[summary]
     Imports task_wait_random then uses an async
     routine called task_wait_n that takes in 2 int arguments (in this order): n
-    and max_delay. You will spawn wait_random n times with the specified max_delay.
-    
+    and max_delay. You will spawn wait_random n times with the specified
+    max_delay.
+
     Args:
         n (int): How many times the async function will be called
         max_delay (int): The max sleep time for each function call
