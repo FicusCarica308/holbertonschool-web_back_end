7c0b6fb4cfa916d5c8b8fc3be30c77d1687f8956
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-10-20 01:05:28 +0000

    added updated comments

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,13 +9,17 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """[summary]
-
+    Imports wait_random from the previous python file then uses an async
+    routine called wait_n that takes in 2 int arguments (in this order): n
+    and max_delay. You will spawn wait_random n times with the specified max_delay.
+    
     Args:
-        n (int): [description]
-        max_delay (int): [description]
+        n (int): How many times the async function will be called
+        max_delay (int): The max sleep time for each function call
 
     Returns:
-        List[float]: [description]
+        List[float]: returns a list of sleep delay times as floats from each
+        async call(await)
     """
     tasks = list()
     delays = list()
