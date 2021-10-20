e4c82648a44c8104e9812232d0d96a7f1c67071f
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-10-20 22:54:06 +0000

    added pycodestyle fixes

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -6,14 +6,15 @@ import asyncio
 import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
+
 async def measure_runtime():
     """[summary]
     """
     start = time.time()
     await asyncio.gather(async_comprehension(),
-                        async_comprehension(),
-                        async_comprehension(),
-                        async_comprehension()
-                        )
+                         async_comprehension(),
+                         async_comprehension(),
+                         async_comprehension()
+                         )
     end = time.time()
     return (end - start)
