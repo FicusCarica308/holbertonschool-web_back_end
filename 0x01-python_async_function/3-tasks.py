d877f82155bca4ea0d118f7feeaad2b1b200e48c
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-10-20 01:20:03 +0000

    added updated comments

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -9,9 +9,10 @@ def task_wait_random(max_delay: int) -> asyncio.Task:
     """[summary]
 
     Args:
-        max_delay (int): [description]
+        max_delay (int): The max sleep length of the wait_random function
 
     Returns:
-        asyncio.Task: [description]
+        asyncio.Task: returns a asyncio task object to be await outside of
+        this functions scope
     """
     return asyncio.create_task(wait_random(max_delay))
