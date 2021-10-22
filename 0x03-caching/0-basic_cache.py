c978edffb3699559aa5a8b161fbee92709ad33b2
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-10-22 19:32:16 +0000

    added basicCache class
    this class overwrites 'get' and 'put' definitions in base_caching.py. This commit does not have proper comments yet

---
+++ 0x03-caching/0-basic_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """[summary]
+    """
+    
+    def put(self, key, item):
+        """[summary]
+
+        Args:
+            key ([type]): [description]
+            item ([type]): [description]
+        """
+        if (key is None or item is None):
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """[summary]
+
+        Args:
+            key ([type]): [description]
+        """
+        if (key is None or key not in self.cache_data):
+            return None
+        return self.cache_data[key]
