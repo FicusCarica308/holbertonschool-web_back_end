779eb9a4c1b5e9d18babc1ba08e24b56c1f60c8d
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-10-23 00:45:03 +0000

    added test version of task 2

---
+++ 0x03-caching/2-lifo_cache.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""[summary]
+"""
+BaseCaching = __import__('0-basic_cache').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """[summary]
+
+    Args:
+        BaseCaching ([type]): [description]
+    """
+    ____last_key_in = None
+
+    def __init__(self):
+        """[summary]
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """[summary]
+
+        Args:
+            key ([type]): [description]
+            item ([type]): [description]
+        """
+        if (key is None or item is None):
+            return
+
+        if (key in self.cache_data):
+            self.cache_data[key] = item
+            self.__last_key_in = key
+            return
+
+        if (len(self.cache_data) >= BaseCaching.MAX_ITEMS):
+            print("DISCARD: {0}".format(self.__last_key_in))
+            self.cache_data.pop(self.__last_key_in)
+
+        self.cache_data[key] = item
+        self.__last_key_in = key
