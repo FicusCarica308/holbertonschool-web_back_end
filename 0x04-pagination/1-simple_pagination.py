87e3155c3af2a1094a8251b526c10b5c0dce07ed
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-10-27 17:12:21 +0000

    added a class that gets and paginates data

--- 0x04-pagination/1-simple_pagination.py
+++ 0x04-pagination/1-simple_pagination.py
@@ -4,19 +4,7 @@
 import csv
 import math
 from typing import List, Tuple
-
-
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """[summary]
-    Args:
-        page (int): [description]
-        page_size (int): [description]
-    Returns:
-        Tuple[int, int]: [description]
-    """
-    start_index = (page * page_size) - page_size
-    end_index = page * page_size
-    return ((start_index, end_index))
+import json
 
 class Server:
     """Server class to paginate a database of popular baby names.
@@ -36,8 +24,34 @@ class Server:
             self.__dataset = dataset[1:]
 
         return self.__dataset
+    
+    def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
+        """[summary]
+        Args:
+            page (int): [description]
+            page_size (int): [description]
+        Returns:
+            Tuple[int, int]: [description]
+        """
+        start_index = (page * page_size) - page_size
+        end_index = page * page_size
+        return ((start_index, end_index))
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            assert page > 0
-            assert page_size > 9
-            
+        """[summary]
+
+        Args:
+            page (int, optional): [description]. Defaults to 1.
+            page_size (int, optional): [description]. Defaults to 10.
+
+        Returns:
+            List[List]: [description]
+        """
+        assert type(page) == int
+        assert type(page_size) == int
+        assert page > 0
+        assert page_size > 0
+        
+        index_range = self.index_range(page, page_size)
+        self.dataset()
+        return (self.__dataset[index_range[0]:index_range[1]])
