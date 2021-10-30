3d5123477681fc9a5c4dd2e9528b382a26c88210
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-10-30 20:01:17 +0000

    added function prototype and type annotations

---
+++ 0x05-personal_data/filtered_logger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""[summary]
+"""
+from typing import List
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str):
+    """[summary]
+
+    Args:
+        fields (List[str]): [description]
+        redaction (str): [description]
+        message (str): [description]
+        separator (str): [description]
+    """
+    pass
