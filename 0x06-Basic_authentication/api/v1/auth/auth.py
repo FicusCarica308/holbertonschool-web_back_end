f3733594d0d41efa307e6b21eec443b9f8d58630
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-11-04 05:09:09 +0000

    added require_auth method implementation

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -18,7 +18,11 @@ class Auth():
         Returns:
             bool: [description]
         """
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path in excluded_paths or path + '/' in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """[summary]
