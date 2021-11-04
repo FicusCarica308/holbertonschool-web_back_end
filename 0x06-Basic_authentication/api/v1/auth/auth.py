d046540b94b7d6b9f77dc0ea182270b1efe3aa9b
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-11-04 16:43:04 +0000

    added implementation to authorization_header class method

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -34,7 +34,11 @@ class Auth():
         Returns:
             str: [description]
         """
-        return None
+        if request is None:
+            return None
+        if request.headers.get('Authorization') is None:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """[summary]
