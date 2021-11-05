40ce750696a1a118c92cbf20827797d362fd53f3
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-11-05 20:49:40 +0000

    added regex to hide certain crednetials

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -7,12 +7,8 @@ import re
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
-    """[summary]
-
-    Args:
-        fields (List[str]): [description]
-        redaction (str): [description]
-        message (str): [description]
-        separator (str): [description]
-    """
-    pass
+    for field in fields:
+        pattern = "{0}=(.*?){1}".format(field, separator)
+        field_value = re.search(pattern, message)
+        message = re.sub(field_value.group(1), redaction, message, 1)
+    return message
