e912b33890a577fc29c970b271f3f326d4901e37
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-11-05 20:53:47 +0000

    added if statment to prevent exception error if field isnt in messsage

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -10,7 +10,8 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     """[summary]
     """
     for field in fields:
-        pattern = "{0}=(.*?){1}".format(field, separator)
-        field_value = re.search(pattern, message)
-        message = re.sub(field_value.group(1), redaction, message, 1)
+        if field in message:
+            pattern = "{0}=(.*?){1}".format(field, separator)
+            field_value = re.search(pattern, message)
+            message = re.sub(field_value.group(1), redaction, message, 1)
     return message
