4ef03fc7af8f4272770d8f81342565de6026677e
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-11-05 03:44:41 +0000

    added check for '*' in require_auth class method

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -18,6 +18,8 @@ class Auth():
         # Handle slashes at end of path / as well as values of excluded_paths
         if path in excluded_paths or path + '/' in excluded_paths:
             return False
+        if path + '*' in excluded_paths:
+            return False
         return True
 
     def authorization_header(self, request=None) -> str:
