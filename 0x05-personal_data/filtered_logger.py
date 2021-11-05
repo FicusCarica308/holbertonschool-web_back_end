ca81a93683b2cded0e7c3ef445a302cc072e3360
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-11-05 21:05:41 +0000

    added pycodestyle fix

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -9,7 +9,7 @@ def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """[summary]"""
     for field in fields:
-            pattern = "{0}=(.*?){1}".format(field, separator)
-            field_value = re.search(pattern, message)
-            message = re.sub(field_value.group(1), redaction, message, 1)
+        pattern = "{0}=(.*?){1}".format(field, separator)
+        field_value = re.search(pattern, message)
+        message = re.sub(field_value.group(1), redaction, message, 1)
     return message
