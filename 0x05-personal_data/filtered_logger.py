59b47753f8e57d800a799fe0869bef1b684ded7a
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-11-05 21:03:12 +0000

    compressed code to make 5 lines

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -2,16 +2,13 @@
 """[summary]
 """
 from typing import List
-import re
+from re import sub, search
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
-    """[summary]
-    """
+    """[summary]"""
     for field in fields:
-        if field in message:
-            pattern = "{0}=(.*?){1}".format(field, separator)
-            field_value = re.search(pattern, message)
-            message = re.sub(field_value.group(1), redaction, message, 1)
+            message = sub(search("{0}=(.*?){1}".format(field, separator),
+                                 message).group(1), redaction, message)
     return message
