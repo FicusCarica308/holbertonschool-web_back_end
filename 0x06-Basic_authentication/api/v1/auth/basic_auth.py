8d14a696d324f4776788f8fe21775ab15eaeec20
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-11-05 03:11:12 +0000

    added class method that finds a User object from the given credentials

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 """
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -55,3 +57,22 @@ class BasicAuth(Auth):
             return None, None
         user_and_pass = decoded_base64_authorization_header.split(':')
         return user_and_pass[0], user_and_pass[1]
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """[summary]
+        """
+        if user_email is None or type(user_email) is not str:
+            return None
+        if user_pwd is None or type(user_pwd) is not str:
+            return None
+        
+        user = User()
+        matching_objects = user.search({'email': user_email})
+        if matching_objects == None or len(matching_objects) == 0:
+            return None
+        for object in matching_objects:
+            if object.is_valid_password(user_pwd) is False:
+                return None
+            else:
+                break
+        return object
