fedd56401238b2d1e56ed43f8ba1ca7b7886ed34
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-11-06 05:16:51 +0000

    added new class and class method implementation
    class method format takes in a record object and uses filter_datum to hide a given list of data fields

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -3,7 +3,7 @@
 """
 from typing import List
 import re
-
+import logging
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
@@ -12,3 +12,23 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         pattern = "(?<={}=).+?(?={})".format(field, separator)
         message = re.sub(pattern, redaction, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """[summary]"""
+        Formatter = logging.Formatter(self.FORMAT)
+        record = Formatter.format(record)
+        filtered = filter_datum(self.fields, self.REDACTION, str(record), self.SEPARATOR)
+        return filtered.format(record)
