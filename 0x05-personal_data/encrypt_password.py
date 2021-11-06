af5cdaea09fcfe0015e94ac05e9ac15d655ecbc7
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-11-06 21:47:16 +0000

    added function called is_valid
    this function checks if a given hashed password is the same as another passed password

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -7,6 +7,17 @@ import bcrypt
 
 def hash_password(password: str) -> bytes:
     """Hashes a given password using bcrypt"""
-    byte_str = bytes(password, 'utf-8')
-    hashed = bcrypt.hashpw(byte_str, bcrypt.gensalt())
+    bytes_password = bytes(password, 'utf-8')
+    hashed = bcrypt.hashpw(bytes_password, bcrypt.gensalt())
     return hashed
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ 
+        Checks if the given hashed password is the same as
+    a given password (used to validate passwords)
+    """
+    bytes_password = bytes(password, 'utf-8') 
+    if bcrypt.checkpw(bytes_password, hashed_password):
+        return True
+    else:
+        return False
