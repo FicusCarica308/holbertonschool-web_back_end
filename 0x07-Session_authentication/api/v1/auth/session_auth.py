00984f0940495424995b55dae4ef834f6c275ca4
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-11-09 02:08:15 +0000

    added create session class method to SessionAuth
    this class method returns a stores a new session for a given user_id and returns its id

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -5,8 +5,19 @@
 
 
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
-    """Curretnyl empty"""
-    pass
+    """Handles session id / session stortage"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a new session for a given user_id (base object id)"""
+        if user_id is None:
+            return None
+        if type(user_id) is not str:
+            return None
+        new_session_id = str(uuid4())
+        self.user_id_by_session_id[new_session_id] = user_id
+        return new_session_id
