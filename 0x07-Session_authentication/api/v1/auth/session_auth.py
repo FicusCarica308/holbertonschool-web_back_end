161c60c291f4226915d29eca6b7f2aa2e6ef50d2
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-11-09 02:17:16 +0000

    added class method called user_id_for_session_id
    will use a passed session id to return the attached user_id

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -21,3 +21,11 @@ class SessionAuth(Auth):
         new_session_id = str(uuid4())
         self.user_id_by_session_id[new_session_id] = user_id
         return new_session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Returns a user_id using the session_id in user_id_by_session_id"""
+        if session_id is None:
+            return None
+        if type(session_id) is not str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
