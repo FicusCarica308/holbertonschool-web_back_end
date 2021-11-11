3c3b3bdb989e2d54038efea59a477fa878048a73
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-11-11 06:37:12 +0000

    pep8 fixes

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -37,7 +37,7 @@ class SessionAuth(Auth):
         session_id = self.session_cookie(request)
         user_id = self.user_id_for_session_id(str(session_id))
         return User.get(user_id)
-    
+
     def destroy_session(self, request=None):
         """ Deletes the user session (logout)"""
         if request is None:
