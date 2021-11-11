3acfc96eef4827c292895d66d7cdc356566c16ab
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-11-11 07:52:12 +0000

    comment fix

--- 0x07-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -36,7 +36,7 @@ class SessionExpAuth(SessionAuth):
         return session_id
 
     def user_id_for_session_id(self, session_id=None):
-        """ """
+        """ Get a user_id from a session_id """
         if session_id is None:
             return None
         if session_id not in self.user_id_by_session_id:
