5de32d8ab1b4c33c23ba9c1d4f187dc4ef700093
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-11-14 21:46:32 +0000

    added database engine with add_user method

---
+++ 0x08-user_authentication_service/db.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+""" 
+    Database control class 
+> Is able to create/delete users
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base
+from user import User
+
+class DB:
+    """ Database engine class (DB)"""
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+    
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """
+        [Summary]
+        > Returns a User object that is simultaneously saved
+        to the database
+
+        Args:
+            email (str): Email for the new user instance
+            hashed_password (str): The new hashed password for the instance
+
+        Returns:
+            User: Returns a User object
+        """
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        return new_user
