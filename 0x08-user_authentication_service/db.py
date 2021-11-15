d631b059d4886ec061d8738724a1f2ced68400cf
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-11-15 02:04:23 +0000

    various changes to fix check failure

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -72,10 +72,9 @@ https://docs.sqlalchemy.org/en/13/orm/query.html#sqlalchemy.orm.query.Query.filt
             If an attribute is passed that isnt in attributes it will
             raise a ValueError
         """
-        DBSession = self._session
         user = self.find_user_by(id=user_id)
         for key, value in kwargs.items():
             if hasattr(user, key) is False:
                 raise ValueError
             setattr(user, key, value)
-        DBSession.commit()
+        self.__session.commit()
