56591a99fa69533d94ad7c04bc138277eee418bf
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-11-16 17:31:13 +0000

    added new method called register_user
    this method creates a new user and adds it to the databse using the DB class in db.py

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """ module containing Authentication handling methods """
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -8,3 +11,24 @@ def _hash_password(password: str) -> bytes:
     bytes_password = bytes(password, 'utf-8')
     hashed_password = bcrypt.hashpw(bytes_password,  bcrypt.gensalt())
     return hashed_password
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """ Init """
+        self._db = DB()
+        
+    def register_user(self, email: str, password: str) -> User:
+        """
+            Temp comment
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError('User {} already exists'.format(email))
+        except NoResultFound:
+            pass
+        hashed_password = _hash_password(password)
+        new_user = self._db.add_user
+        return new_user
