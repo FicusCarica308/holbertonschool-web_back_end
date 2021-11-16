c65c922a04141c70f3e7eadabd527e51c25ea56c
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-11-16 00:35:30 +0000

    added new file called auth.py with new method _hash_password()

---
+++ 0x08-user_authentication_service/auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" module containing Authentication handling methods """
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """ returns a salted password hashed using bcrypt """
+    bytes_password = bytes(password, 'utf-8')
+    hashed_password = bcrypt.hashpw(bytes_password,  bcrypt.gensalt())
+    return hashed_password
