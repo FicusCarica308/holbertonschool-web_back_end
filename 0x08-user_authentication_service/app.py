1aefb206786adc69d3981abb29b0818b3504bbcb
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-11-16 23:58:18 +0000

    added a minimal flask application

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python3
 """ Flask application file """
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route('/')
+def route_endpoint():
+    """ Route handler """
+    return jsonify({"message": "Bienvenue"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
