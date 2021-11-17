ce59c8ca5b9173581f936e7dee9c22d2320eece0
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-11-17 02:07:26 +0000

    added new Auth class method destroy_session()

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -76,3 +76,11 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """ sets a users session_id back to None based on a user_id """
+        try:
+            user = self._db.find_user_by(id=user_id)
+            self._db.update_user(session_id=None)
+        except NoResultFound:
+            return None
