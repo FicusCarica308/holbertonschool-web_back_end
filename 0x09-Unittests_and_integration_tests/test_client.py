f6352302429cf60e30c3283bc6557342d0832500
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-11-23 17:23:01 +0000

    added pep8 fixes

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -4,6 +4,7 @@ import unittest
 from unittest.mock import Mock, patch
 from parameterized import parameterized
 
+
 class TestGithubOrgClient(unittest.TestCase):
     """ Tests for all tGithubOrgClient class methods """
 
@@ -16,7 +17,7 @@ class TestGithubOrgClient(unittest.TestCase):
         patch_res.json.return_value = test_payload
         patch_res.json.assert_called_once()
         print(patch_res.json)
-        pass
-    
+
+
 if __name__ == "__main__":
     unittest.main()
