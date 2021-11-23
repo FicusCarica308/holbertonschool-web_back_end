8586bba513764f87f39f07c6ca169466aa72a4c8
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-11-23 18:05:34 +0000

    added test_public_repos_url()

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ More unittests """
 import unittest
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -12,15 +12,23 @@ class TestGithubOrgClient(unittest.TestCase):
     @parameterized.expand([("google", {"payload": True}),
                            ("abc", {"payload": False})])
     @patch('client.get_json')
-    def test_org(self, test, test_payload, patch_res):
+    def test_org(self, test, test_payload, patch):
         """ Tests 'org' class method """
         # patch setup
-        patch_res.return_value = test_payload
+        patch.return_value = test_payload
         
         client = GithubOrgClient(test)
         self.assertEqual(client.org, test_payload)
-        patch_res.assert_called_once()
+        patch.assert_called_once()
 
+    @patch('client.GithubOrgClient.org', new_callable=PropertyMock)
+    def test_public_repos_url(self, patch):
+        """ Tests '_public_repos_url' class method that
+        has been turned into a property by 'memoize' """
+        patch.return_value = {"repos_url": "test_value"}
+        client = GithubOrgClient('abc')
+        result = client._public_repos_url
+        self.assertEqual(result, "test_value")
 
 if __name__ == "__main__":
     unittest.main()
