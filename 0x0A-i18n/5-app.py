1cfe873d6b85ff73f15e978a292d0e7418b81b5a
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-11-28 18:46:03 +0000

    added task 5

---
+++ 0x0A-i18n/5-app.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+""" Very simple flask app using babel """
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+
+class Config():
+    """ Configuration for babel translation """
+    LANGUAGES = ["en", "fr"]
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+app = Flask(__name__)
+app.config.from_object(Config())
+Babel.default_locale = "en"
+Babel.default_timezone = "UTC"
+babel = Babel(app)
+
+
+def get_user():
+    id = request.args.get('login_as')
+    return users.get(int(id))
+
+
+@app.before_request
+def before_request_handler():
+    """ Before request """
+    user = get_user()
+    g.user = user
+
+@babel.localeselector
+def get_locale():
+    """ Determine the best match with our supported languages """
+    lang = request.args.get('locale')
+    if lang is not None:
+        if lang in app.config['LANGUAGES']:
+            return lang
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def holby_welcome():
+    """ Renders 0-index.html from templates directory """
+    return render_template('5-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
