70f554062fb53007e6194f0d49850fb577b5636a
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-11-28 16:42:01 +0000

    added default locale and timezone

---
+++ 0x0A-i18n/1-app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" Very simple flask app using babel """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app, default_locale='en', default_timezone='UTC')
+
+
+class Config():
+    """ Configuration for babel translation """
+    LANGUAGES = ["en", "fr"]
+
+@app.route("/")
+def holby_welcome():
+    """ Renders 0-index.html from templates directory """
+    return render_template('0-index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
