f323a700aaca88251740b195fa4774385554ef89
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-11-28 17:50:58 +0000

    changed what index files is rendered

---
+++ 0x0A-i18n/3-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" Very simple flask app using babel """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config():
+    """ Configuration for babel translation """
+    LANGUAGES = ["en", "fr"]
+
+
+app = Flask(__name__)
+app.config.from_object(Config())
+Babel.default_locale = "en"
+Babel.default_timezone = "UTC"
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """ Determine the best match with our supported languages. """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def holby_welcome():
+    """ Renders 0-index.html from templates directory """
+    return render_template('3-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
