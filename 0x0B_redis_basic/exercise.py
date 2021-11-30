332a1684407557da7742c9b7e097ed2faa878065
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-11-30 02:31:44 +0000

    added get() class method
    this feature allows our redis cache to get a given keys value and convert it out of a bytes() string

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """ Redis practice module """
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache():
@@ -22,3 +22,18 @@ class Cache():
         random_key = str(random_key)
         self._redis.mset({random_key: data})
         return random_key
+
+    def get(self, key: str, fn: Callable = None):
+        """
+        Returns a value from a given key in the Redis cache...
+        Optional argument 'fn' can be used to convert the returned
+        value into a requested type.
+
+        EXP (int): Cache.get(key, int) -> returns integer
+        Cache.store("example_key", 123)
+        Cache.get("example_key", int) -> returns 123 instead of b"123"
+        """
+        value = self._redis.get(key)
+        if fn is not None and value is not None:
+            return fn(value)
+        return value
