3c2e4f172fd5e756e93fdef3aa0583e5341edfa9
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-12-02 23:43:45 +0000

    makde id a primary key

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,8 @@
 -- a SQL script that creates a table users following certain requirments
 -- Creates the TABLE
-CREATE TABLE users IF NOT EXISTS(
-    id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE users IF NOT EXISTS (
+    id INT AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
+    name VARCHAR(255),
+    PRIMARY KEY (id)
 )
