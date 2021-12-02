7079c4a7ab0943f3677fc233ba68682b8dc21930
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-12-02 23:51:39 +0000

    added task 1

---
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- creates a users table with given requirments
+-- creates the table
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM ('US', 'CO', 'TN') DEFAULT 'US'
+    PRIMARY KEY (id)
+)
