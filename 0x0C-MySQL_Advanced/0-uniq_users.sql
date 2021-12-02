cf1970aca4787420181281d8c1eed0b3975241a2
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-12-02 23:38:23 +0000

    added first SQL script

---
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- a SQL script that creates a table users following certain requirments
+-- Creates the TABLE
+CREATE TABLE users IF NOT EXISTS(
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE
+    name VARCHAR(255)
+)
