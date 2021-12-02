b1e144563d5cdd49e64cf036ad0fc6e758dc86f6
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-12-02 23:45:31 +0000

    added change to CREATE TABLE call

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- a SQL script that creates a table users following certain requirments
 -- Creates the TABLE
-CREATE TABLE users IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
