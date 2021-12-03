e54be3d10c54705895f7574201bd1ee241f06ae6
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-12-03 02:27:48 +0000

    added task 6

---
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,13 @@
+-- Temp
+-- Temp
+
+DELIMITER $$
+
+CREATE PROCEDURE AddBonus (IN user_id INT, IN project_name VARCHAR(255), IN score FLOAT)
+BEGIN
+    -- IF ((SELECT count(name) FROM projects WHERE name = project_name) = 0)
+    -- THEN
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, (SELECT id FROM projects WHERE name = project_name), score);
+END
+$$
+DELIMITER ;
