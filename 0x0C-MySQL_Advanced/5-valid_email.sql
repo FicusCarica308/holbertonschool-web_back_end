404de885ca5952ae31f642987aa19927dc1106ed
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-12-03 02:00:15 +0000

    added task 5

---
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,11 @@
+-- creates a new TRIGGER
+-- a trigger that resets the attribute valid_email only when the email has been changed
+DELIMITER $$
+CREATE TRIGGER email_validation
+AFTER UPDATE ON users FOR EACH ROW
+BEGIN
+IF NEW.email != OLD.email THEN
+UPDATE user SET valid_email = 0 WHERE email = NEW.email;
+END IF;
+END;
+$$
