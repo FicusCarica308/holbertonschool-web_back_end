572cb8f7a12b72ab514d4ee56374c8c86f003456
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-12-03 02:43:47 +0000

    added comments to task6

--- 0x0C-MySQL_Advanced/6-bonus.sql
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -1,6 +1,5 @@
--- Temp
--- Temp
-DROP PROCEDURE IF EXISTS AddBonus;
+-- TASK : Add bonus
+-- creates a stored procedure AddBonus that adds a new correction for a student.
 DELIMITER $$
 
 CREATE PROCEDURE AddBonus (IN user_id INT, IN project_name VARCHAR(255), IN score FLOAT)
