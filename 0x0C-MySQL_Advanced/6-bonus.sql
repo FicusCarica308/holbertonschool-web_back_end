70228717f9a4ef5d2f89165b4893149fc6e95009
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-12-03 02:35:11 +0000

    added limit to how many project_id 's can be returned

--- 0x0C-MySQL_Advanced/6-bonus.sql
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -5,11 +5,11 @@ DELIMITER $$
 
 CREATE PROCEDURE AddBonus (IN user_id INT, IN project_name VARCHAR(255), IN score FLOAT)
 BEGIN
-    IF (SELECT COUNT(name) FROM projects WHERE name = project_name) = 0
+    IF ((SELECT COUNT(name) FROM projects WHERE name = project_name) = 0)
     THEN
         INSERT INTO proejcts (name) VALUES (project_name);
     END IF;
-    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, (SELECT id FROM projects WHERE name = project_name), score);
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, (SELECT id FROM projects WHERE name = project_name LIMIT 1), score);
 END
 $$
 DELIMITER ;
