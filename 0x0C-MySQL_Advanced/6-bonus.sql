67fa92bb4911d0b64a1c61c428e1925c6a632ea4
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-12-03 02:34:20 +0000

    removed parenthesis from if statment on line 8

--- 0x0C-MySQL_Advanced/6-bonus.sql
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -5,7 +5,7 @@ DELIMITER $$
 
 CREATE PROCEDURE AddBonus (IN user_id INT, IN project_name VARCHAR(255), IN score FLOAT)
 BEGIN
-    IF ((SELECT COUNT(*) FROM projects WHERE name = project_name) = 0)
+    IF (SELECT COUNT(name) FROM projects WHERE name = project_name) = 0
     THEN
         INSERT INTO proejcts (name) VALUES (project_name);
     END IF;
