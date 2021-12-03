fdcc0c5e4859314a3eb0c761500ffc5de4b80abf
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-12-03 03:10:54 +0000

    added task 7:

---
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,11 @@
+-- .....
+-- creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student.
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER $$
+
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+BEGIN
+    UPDATE users SET average_score = CAST((SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id) AS FLOAT) WHERE id=user_id;
+END
+$$
+DELIMITER ;
