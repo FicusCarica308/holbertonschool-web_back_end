9ced2be7e616cebaa4ed78366b757cbe52b01680
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-12-03 02:41:22 +0000

    fixed typo on line 9

--- 0x0C-MySQL_Advanced/6-bonus.sql
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -1,15 +1,15 @@
 -- Temp
 -- Temp
-
+DROP PROCEDURE IF EXISTS AddBonus;
 DELIMITER $$
 
 CREATE PROCEDURE AddBonus (IN user_id INT, IN project_name VARCHAR(255), IN score FLOAT)
 BEGIN
     IF ((SELECT COUNT(name) FROM projects WHERE name = project_name) = 0)
     THEN
-        INSERT INTO proejcts (name) VALUES (project_name);
+        INSERT INTO projects (name) VALUES (project_name);
     END IF;
-    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, (SELECT id FROM projects WHERE name = project_name LIMIT 1), score);
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, (SELECT id FROM projects WHERE name = project_name), score);
 END
 $$
 DELIMITER ;
