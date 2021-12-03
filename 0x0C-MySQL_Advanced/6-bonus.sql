36ed2a23df1f810f528ee7afd7ca64548bf83733
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-12-03 02:30:04 +0000

    added sections that creates a project if the project doesnt exist

--- 0x0C-MySQL_Advanced/6-bonus.sql
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -5,8 +5,10 @@ DELIMITER $$
 
 CREATE PROCEDURE AddBonus (IN user_id INT, IN project_name VARCHAR(255), IN score FLOAT)
 BEGIN
-    -- IF ((SELECT count(name) FROM projects WHERE name = project_name) = 0)
-    -- THEN
+    IF ((SELECT count(name) FROM projects WHERE name = project_name) = 0)
+    THEN
+        INSERT INTO proejcts (name) VALUES (project_name);
+    END IF;
     INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, (SELECT id FROM projects WHERE name = project_name), score);
 END
 $$
