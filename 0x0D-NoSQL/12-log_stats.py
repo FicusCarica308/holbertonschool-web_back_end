a1290b7eeabc60cd0b8c7ed314d2e2fe1fddcd68
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-12-08 01:00:07 +0000

    added task 12

---
+++ 0x0D-NoSQL/12-log_stats.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Task(12): a Python script that provides some stats
+about Nginx logs stored in MongoDB """
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    """ Script """
+    DB_CLIENT = MongoClient()
+    logs_db = DB_CLIENT.logs
+    nginx_collection = logs_db.nginx
+    document_count = nginx_collection.find().count()
+    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("{} logs".format(document_count))
+    print("Methods:")
+    for meth in method:
+        print("{}: {}".format(meth, nginx_collection.find({"method": meth}).count()))
+    
