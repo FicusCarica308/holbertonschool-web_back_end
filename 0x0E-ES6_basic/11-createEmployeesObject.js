b3a40178e98e99caa256e302c05e9b799a7c8c6a
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-12-10 04:51:09 +0000

    fixed eslint errors

--- 0x0E-ES6_basic/11-createEmployeesObject.js
+++ 0x0E-ES6_basic/11-createEmployeesObject.js
@@ -1,3 +1,3 @@
 export default function createEmployeesObject(departmentName, employees) {
-  return {[departmentName]: Array.from(employees)};
+  return { [departmentName]: Array.from(employees) };
 }
