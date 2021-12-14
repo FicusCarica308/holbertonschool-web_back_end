14a72a1cb5af7d5eb098b6af33e1fe59cf94b573
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-12-14 04:51:16 +0000

    added task 9

---
+++ 0x0F-ES6_promise/9-try.js
@@ -0,0 +1,11 @@
+/* Task(9) */
+
+export default function guardrail(mathFunction) {
+  const queue = ['Guardrail was processed'];
+  try {
+    queue.unshift(mathFunction());
+  } catch (error) {
+    queue.unshift(error.toString());
+  }
+  return (queue);
+}
