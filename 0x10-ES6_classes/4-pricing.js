4411d7bb5b06bcaabf51c028559a764c0fe2d8f4
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-12-17 01:57:58 +0000

    added task 4

---
+++ 0x10-ES6_classes/4-pricing.js
@@ -0,0 +1,32 @@
+// Task(4): Implement a class named Pricing
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  get currency() {
+    return this._currenct;
+  }
+
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this._currency.name} (${this._currency.code})`
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
