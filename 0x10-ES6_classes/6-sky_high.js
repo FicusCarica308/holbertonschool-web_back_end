a324d2dfbbf8ecb64663d17df1a0a95a98eb0a0c
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-12-17 03:15:47 +0000

    added task6

--- 0x10-ES6_classes/6-sky_high.js
+++ 0x10-ES6_classes/6-sky_high.js
@@ -7,11 +7,15 @@ export default class SkyHighBuilding extends Building {
     this._floors = floors;
   }
 
+  get sqft() {
+    return this._sqft;
+  }
+
   get floors() {
     return this._floors;
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this.floors} floors`;
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
