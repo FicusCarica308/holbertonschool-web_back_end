6d4b9932c20396f5104afdffc393d076c892f714
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-12-17 02:39:12 +0000

    added task 5

---
+++ 0x10-ES6_classes/5-building.js
@@ -0,0 +1,20 @@
+// Task(5) Implement a class named Building
+
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+    if (this.constructor !== Building) {
+      if (this.evacuationWarningMessage !== "undefined") {
+        throw Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    this._sqft = newSqft;
+  }
+}
