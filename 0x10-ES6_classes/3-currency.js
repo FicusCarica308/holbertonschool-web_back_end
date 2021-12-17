bd20223eabe2d3561fa76ccfccc075abaee83cd7
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-12-17 01:49:02 +0000

    added task 3

---
+++ 0x10-ES6_classes/3-currency.js
@@ -0,0 +1,28 @@
+// Task(3): Implement a class named Currency (no typing)
+
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    this._code = newCode;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
