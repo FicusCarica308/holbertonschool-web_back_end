b4106e7e4ebee69e14e14daff4b6b90f39c9b150
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-12-20 23:45:30 +0000

    added task 2 function file

---
+++ 0x11-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,8 @@
+// Task(1): that returns an array of objects who are located in a specific city.
+
+export default function getStudentsByLocation(students, city) {
+  /* if (students instanceof Array === false) {
+    return [];
+  } */
+  return (students.filter((student) => student.location === city));
+}
