de095911962854839700d38ff180d5470f19ad09
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-12-20 23:54:37 +0000

    added task 3 function file

---
+++ 0x11-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,8 @@
+// Task(3): a function getStudentIdsSum that returns the sum of all the student ids.
+
+export default function getStudentsByLocation(students) {
+  /* if (students instanceof Array === false) {
+    return [];
+  } */
+  return (students.reduce((prev, curr) => prev + curr.id, 0));
+}
