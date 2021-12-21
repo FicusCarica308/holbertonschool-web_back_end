2f328d12133228fb710131c5d220f7caf2c12901
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-12-21 02:10:37 +0000

    added task 9

---
+++ 0x11-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,15 @@
+/*
+  Task(9): a function named groceriesList that returns a map
+  of groceries
+*/
+export default function groceriesList() {
+  const newMap = new Map();
+
+  newMap.set('Apples', 10);
+  newMap.set('Tomatoes', 10);
+  newMap.set('Pasta', 1);
+  newMap.set('Rice', 1);
+  newMap.set('Banana', 5);
+
+  return newMap;
+}
