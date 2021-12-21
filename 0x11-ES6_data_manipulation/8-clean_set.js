d9ca7534b0a43ad65985a6c669c68932206bd0ec
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-12-21 17:24:15 +0000

    added another edgecase for set not being a Set

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -4,6 +4,7 @@
 */
 export default function cleanSet(set, startString) {
   if (startString === '') return ('');
+  if (set instanceof Set === false) return ('');
   const editedStrings = [];
   set.forEach((value) => {
     if (value.startsWith(startString)) {
