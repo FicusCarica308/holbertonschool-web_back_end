b5aad61c1f5ed1e1ba626c85b97f19ad48a9367f
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-12-21 00:25:19 +0000

    changed 'firstname' to 'firstName' in all obj's

--- 0x11-ES6_data_manipulation/0-get_list_students.js
+++ 0x11-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,8 @@
 // Task(0): Create a function named getListStudents that returns an array of objects
 
 export default function getListStudents() {
-  const obj1 = { id: 1, firstname: 'Guillaume', location: 'San Francisco' };
-  const obj2 = { id: 2, firstname: 'James', location: 'Columbia' };
-  const obj3 = { id: 5, firstname: 'Serena', location: 'San Francisco' };
+  const obj1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+  const obj2 = { id: 2, firstName: 'James', location: 'Columbia' };
+  const obj3 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
   return ([obj1, obj2, obj3]);
 }
