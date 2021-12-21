f20291b3f4672f51bce53764c88764923b3539e3
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-12-21 00:30:20 +0000

    added task 4

---
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,18 @@
+/*
+Task(4): a function updateStudentGradeByCity that returns an array of students
+for a specific city with their new grade
+*/
+
+export default function getStudentsByLocation(students, city, newGrades) {
+  const callBack = (student) => {
+    const updatedStudent = student;
+    const gradeForId = newGrades.filter((grade) => grade.studentId === student.id);
+    if (gradeForId.length >= 1) {
+      updatedStudent.grade = gradeForId[0].grade;
+    } else {
+      updatedStudent.grade = 'N/A';
+    }
+    return updatedStudent;
+  };
+  return (students.filter((student) => student.location === city).map(callBack));
+}
