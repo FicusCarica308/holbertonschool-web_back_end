20af4178db0c59bf967c7fdafdc71fd38de9c15c
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-12-21 17:13:13 +0000

    added eslint error fixes

--- 0x11-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x11-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,7 @@
-// Task(2): a function getStudentsByLocation that returns an array of objects who are located in a specific city.
+/*
+  Task(2): a function getStudentsByLocation that returns an array
+  of objects who are located in a specific city.
+*/
 
 export default function getStudentsByLocation(students, city) {
   /* if (students instanceof Array === false) {
