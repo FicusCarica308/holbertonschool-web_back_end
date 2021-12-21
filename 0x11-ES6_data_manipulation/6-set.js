50bb81a47cef62bce11c1ae0e2b86ab6c7d4db47
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-12-21 01:13:19 +0000

    added task 6

---
+++ 0x11-ES6_data_manipulation/6-set.js
@@ -0,0 +1,7 @@
+// Task(6): a function named setFromArray that returns a Set from an array
+export default function setFromArray(array) {
+  const set1 = new Set();
+  for (const item of array)
+    set1.add(item);
+  return (set1);
+}
