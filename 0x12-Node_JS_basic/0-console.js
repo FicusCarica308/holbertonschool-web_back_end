fb50a9fe6e827c943ecceeeb6d989f11cfdbcccd
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-01-02 01:38:39 +0000

    added new function called displayMessage() in module 0-console.js
    this function displays a string argument to STDOUT

--- 0x12-Node_JS_basic/0-console.js
+++ 0x12-Node_JS_basic/0-console.js
@@ -1,5 +1,7 @@
 // Task (0) - create a function named displayMessage that prints in STDOUT the string argument.
 
-export default function displayMessage(str) {
+function displayMessage(str) {
     console.log(str);
 }
+
+module.exports = displayMessage;
