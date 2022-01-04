0dab0094e13492b87b266321af35904ddf83ab97
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-01-04 01:42:03 +0000

    rewrote 2-reade_file to work async

---
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,40 @@
+// Task (2) - using database.csv, create a function countStudents that attempts to read the
+// database file synchronously
+
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, lines) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
+        return;
+      }
+      const fields = {};
+      let overallCount = 0;
+      const data = lines.split('\n');
+      for (const person of data) {
+        if (person !== '' && data.indexOf(person) !== 0) {
+          const personData = person.split(',');
+          const personField = personData.slice(-1)[0];
+          overallCount += 1;
+          if (personField in fields) {
+            fields[personField].count += 1;
+            fields[personField].names.push(personData[0]);
+          } else {
+            fields[personField] = { count: 1, names: [personData[0]] };
+          }
+        }
+      }
+      console.log(`Number of students: ${overallCount}`);
+      for (const key in fields) {
+        if (key in fields) {
+          console.log(`Number of students in ${key}: ${fields[key].count}. List: ${fields[key].names.join(', ')}`);
+        }
+      }
+      resolve();
+    });
+  });
+}
+
+module.exports = countStudents;
