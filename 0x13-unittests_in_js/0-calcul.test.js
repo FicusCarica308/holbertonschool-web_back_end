b0d5d34331c4d58d7f9e0ba56a27c4baab2fa29f
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-01-05 17:53:47 +0000

    seperated all four assertions to their own tests

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -7,8 +7,14 @@ const calculateNumber = require("./0-calcul.js");
 describe("Round test", function() {
   it("checks if a and b are rounded and added together properly", function() {
     assert.equal(calculateNumber(1, 3), 4);
+  });
+  it("checks if a and b are rounded and added together properly", function() {
     assert.equal(calculateNumber(1, 3.7), 5);
+  });
+  it("checks if a and b are rounded and added together properly", function() {
     assert.equal(calculateNumber(1.2, 3.7), 5);
+  });
+  it("checks if a and b are rounded and added together properly", function() {
     assert.equal(calculateNumber(1.5, 3.7), 6);
   });
 });
