d59ae9573114960125e2cae0d2c8bb68c446897a
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-01-05 22:32:54 +0000

    added done() to it() function callback

--- 0x13-unittests_in_js/8-api/api.test.js
+++ 0x13-unittests_in_js/8-api/api.test.js
@@ -6,10 +6,11 @@ const sinon = require("sinon");
 const request = require('request');
 
 describe('Index page', () => {
-  it('returns status code 200', () => {
+  it('returns status code 200', (done) => {
     request('http://localhost:7865', (err, res, body) => {
       expect(res.statusCode).to.equal(200);
       expect(body).to.equal('Welcome to the payment system');
     });
+    done();
   });
 });
