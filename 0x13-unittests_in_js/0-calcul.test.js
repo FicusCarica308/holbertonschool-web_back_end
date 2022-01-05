ec76ce075bec6a9ea430b56fe0336d66fec8769c
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-01-05 18:12:34 +0000

    added a test for negative numbers

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -5,16 +5,16 @@ const calculateNumber = require("./0-calcul.js");
 
 
 describe("calculateNumber", function() {
-  it("checks if a and b are rounded and added together properly", function() {
+  it("checks if numbers are rounded and added together properly using integers", function() {
     assert.equal(calculateNumber(1, 3), 4);
   });
-  it("checks if a and b are rounded and added together properly", function() {
+  it("checks if decimal numbers are rounded and added together properly", function() {
     assert.equal(calculateNumber(1, 3.7), 5);
-  });
-  it("checks if a and b are rounded and added together properly", function() {
     assert.equal(calculateNumber(1.2, 3.7), 5);
-  });
-  it("checks if a and b are rounded and added together properly", function() {
     assert.equal(calculateNumber(1.5, 3.7), 6);
+  });
+  it("checks if negative numbers are rounded and added together properly", function() {
+    assert.equal(calculateNumber(-1.2, -3.7), -5);
+    assert.equal(calculateNumber(-3, -6), -9);
   });
 });
