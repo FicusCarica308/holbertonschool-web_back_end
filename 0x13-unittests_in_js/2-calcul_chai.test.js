bf2c242cdfe08b95ecdb6f526691a993ddc02406
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-01-05 18:59:44 +0000

    changed calculateNumber import file path

--- 0x13-unittests_in_js/2-calcul_chai.test.js
+++ 0x13-unittests_in_js/2-calcul_chai.test.js
@@ -2,7 +2,7 @@
 
 const assert = require("assert");
 const { expect } = require("chai");
-const calculateNumber = require("./2-calcul.js");
+const calculateNumber = require("./2-calcul_chai.js");
 
 
 describe("calculateNumber", function() {
