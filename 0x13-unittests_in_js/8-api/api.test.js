d82c7b74b0e9c9c70e54b6d8c5705e925151c344
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-01-05 22:27:36 +0000

    added expect() for root response check

--- 0x13-unittests_in_js/8-api/api.test.js
+++ 0x13-unittests_in_js/8-api/api.test.js
@@ -9,6 +9,7 @@ describe('Index page', () => {
   it('returns status code 200', () => {
     request('http://localhost:7865', (err, res, body) => {
       expect(res.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
     });
   });
 });
