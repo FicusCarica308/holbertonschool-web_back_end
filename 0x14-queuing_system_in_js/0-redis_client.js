0014366497250433d52ccbde8de5aa16217356b4
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-01-09 02:17:05 +0000

    added script to connect nodejs to a local redis cleint

---
+++ 0x14-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,14 @@
+/* Task (0) - connects to a local redis client
+* Displays a message on connect and disconnect using node-redis client class
+*/
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err}`);
+});
